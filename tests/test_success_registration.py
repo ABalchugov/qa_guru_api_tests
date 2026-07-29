c280da76c51b8e800b53609a8c914127dc5d8156
ABalchugov <inspektr1994@gmail.com>
2026-07-29 21:15:48 +0300

    feature add test successful registration

---
+++ tests/test_success_registration.py
@@ -0,0 +1,32 @@
+import requests
+
+from jsonschema import validate
+from schemas.registration_schema import registration_schema
+
+API_URL = "https://book-club.qa.guru/api/v1"
+USERNAME = "avbalchugov"
+PASSWORD = "password"
+
+def test_successful_registration():
+    request_body = {"username": USERNAME, "password": PASSWORD}
+
+    response = requests.post(f"{API_URL}/users/register/", json=request_body)
+
+    print("\nStatus code:", response.status_code)
+    print("Headers:", response.headers)
+    print("Body:", response.text)
+
+    assert response.status_code == 201
+
+    body = response.json()
+    validate(body, schema=registration_schema)
+
+    assert body["username"] == request_body["username"]
+
+    response_auth_token = requests.post(f"{API_URL}/auth/token/", json=request_body)
+
+    auth_token_body = response_auth_token.json()
+    auth_token = auth_token_body["access"]
+
+    response_delete_user = requests.delete(f"{API_URL}/users/me/", headers={"Authorization": "Bearer " + auth_token})
+    assert response_delete_user.status_code is 204
